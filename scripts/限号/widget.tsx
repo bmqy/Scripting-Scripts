cff97e0c9dc6aa417f15db60d8e2cc15ab64d78c
bmqy <bmqy@qq.com>
2025-10-29 13:21:36 +0800

    update

--- scripts/限号/widget.tsx
+++ scripts/限号/widget.tsx
@@ -1,5 +1,6 @@
 // 限号助手小组件 - 主文件
-import { Circle, HStack, Image, RoundedRectangle, Spacer, Text, VStack, Widget, ZStack } from "scripting"
+import * as scripting from "scripting"
+const { Circle, HStack, Image, RoundedRectangle, Spacer, Text, VStack, Widget, ZStack } = scripting
 // 导入拆分出去的模块
 import { getCurrentTime, getShortLimitInfo } from './utils/base'
 import { getLimitNumbers, getWeeklyLimitNumbers } from './utils/service'
