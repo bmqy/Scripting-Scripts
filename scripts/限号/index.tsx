a3c37b6c98d6f59752e34577c46f260a2bb1c45d
bmqy <bmqy@qq.com>
2025-10-29 13:10:04 +0800

    update

--- scripts/限号/index.tsx
+++ scripts/限号/index.tsx
@@ -1,3 +1,4 @@
 // 在主脚本中清除今日的限号缓存
-import { Storage } from 'scripting'
+import * as scripting from "scripting"
+const { Storage } = scripting
 Storage.remove(`limitNumbers_${new Date().toLocaleDateString()}`)
