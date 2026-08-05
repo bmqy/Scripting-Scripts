18762a1cb0b3962c4ce482f6335f1104032eb692
bmqy <bmqy@qq.com>
2026-08-05 22:18:06 +0800

    微幅上移 RSS 阅读标题元信息

--- scripts/RSS阅读/index.tsx
+++ scripts/RSS阅读/index.tsx
@@ -1955,7 +1955,7 @@ function SettingsPage() {
         listRowSeparator="hidden"
       >
         <Text font="largeTitle" fontWeight="bold">RSS 阅读</Text>
-        <HStack alignment="lastTextBaseline" spacing={6} offset={{ x: 0, y: 6 }}>
+        <HStack alignment="lastTextBaseline" spacing={6} offset={{ x: 0, y: 3 }}>
           <Link url={GITHUB_REPOSITORY_URL}>
             <Image
               imageUrl="https://github.com/favicon.ico"
